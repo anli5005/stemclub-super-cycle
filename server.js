7ffa81511a00021123a94e39d2fae441263ca916
anli5005 <li.anthony917@gmail.com>
2015-11-17 19:52:50 -0500

    Add log messages

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ var config = {
   programs: {
     ultrasonic: {command: "python", args: ["/home/pi/Documents/PythonTest/ultrasonic.py"]
   }
-}
+};
+
+console.log("Setting up child processes");
 
 var child = require("child_process");
 
+console.log("Setting up ultrasonic sensor");
 var ultrasonic = child.spawn(config.programs.ultrasonic.command, config.programs.ultrasonic.args);
 ultrasonic.on("error", function(error) {
-  console.warn("Failed to launch ultrasonic sensor")
+  console.warn("Failed to launch ultrasonic sensor program");
 });
