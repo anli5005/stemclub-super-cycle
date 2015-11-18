083e9b54bb2e4351a3c069bdadaf9fe6fdf00185
anli5005 <li.anthony917@gmail.com>
2015-11-17 19:48:09 -0500

    Add ultrasonic sensor functionality to server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.warn("Implementation nonexistent.");
+var config = {
+  programs: {
+    ultrasonic: {command: "python", args: ["/home/pi/Documents/PythonTest/ultrasonic.py"]
+  }
+}
+
+var child = require("child_process");
+
+var ultrasonic = child.spawn(config.programs.ultrasonic.command, config.programs.ultrasonic.args);
+ultrasonic.on("error", function(error) {
+  console.warn("Failed to launch ultrasonic sensor")
+});
